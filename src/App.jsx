c9c00116ce844f2fe1d089bc421f535c323fa283
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-28 16:34:24 -0300

    feat: crear rutas al resto de páginas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,26 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
+import { Route, Routes } from 'react-router'
+import Inicio from './pages/Inicio'
+import Productos from './pages/Productos'
+import DetalleProducto from './pages/DetalleProducto'
+import Carrito from './pages/Carrito'
+import Contacto from './pages/Contacto'
+import Error404 from './pages/Error404'
+
 function App() {
 
   return (
     <>
-      
+    <Routes>
+      <Route path='/' element={Inicio}/>
+      <Route path='/catalogo' element={Productos}/>
+      <Route path='/catalogo/producto/:id' element={DetalleProducto}/>
+      <Route path='/carrito' element={Carrito}/>
+      <Route path='/contacto' element={Contacto}/>
+      <Route path='*' element={Error404}/>
+    </Routes>
     </>
   )
 }
