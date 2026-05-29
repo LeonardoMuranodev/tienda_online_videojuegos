1082504f475df253d39f7d8884288209927c267e
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-29 14:16:04 -0300

    chore: instalar e importar tailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-
 import { Route, Routes } from 'react-router'
+
+import './App.css'
+
 import Inicio from './pages/Inicio.jsx'
 import Productos from './pages/Productos.jsx'
 import DetalleProducto from './pages/DetalleProducto.jsx'
