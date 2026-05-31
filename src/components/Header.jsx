e9bdfe70b2fe895c2b89c2b5e68b32e4536f7ac1
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-31 17:37:29 -0300

    feature: implementación parcial de navbar utilizando componentes de flowbite

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
-import { NavLink } from "react-router"
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react"
+import { Form, NavLink } from "react-router"
 
 function Header() {
     const navClass = "p-1 hover:font-bold active:font-bold"
     return (
         <header>
-            <div className="flex justify-between p-4 bg-gray-950 text-amber-100">
-                <NavLink to='/'>Logo</NavLink>
-                <div>barra de busqueda</div>
-                <NavLink to='/carrito'>carrito</NavLink>
-            </div>
-            <div className="flex justify-start p-1 bg-gray-500 text-amber-100">
-                <NavLink to='/' className={navClass}>Inicio</NavLink>
-                <NavLink to='/catalogo' className={navClass}>Catálogo</NavLink>
-                <NavLink to='/catalogo/novedades' className={navClass}>Novedades</NavLink>
-                <NavLink to='/catalogo/ofertas' className={navClass}>Ofertas</NavLink>
-            </div>
+            <Navbar fluid>
+                <NavbarBrand as={NavLink} to='/'>
+                    Logo
+                </NavbarBrand>
+                <NavbarLink as={NavLink} to='carrito'>Carrito</NavbarLink>
+            </Navbar>
+            <Navbar fluid>
+                <NavbarToggle/>
+                <NavbarCollapse>
+                    <NavbarLink as={NavLink} to='/'>Inicio</NavbarLink>
+                    <NavbarLink as={NavLink} to='/catalogo'>Catálogo</NavbarLink>
+                </NavbarCollapse>
+            </Navbar>
         </header>
     )
 }
