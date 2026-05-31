2988c59bef1f17eb6b74a60ad44b67b47259f176
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-05-31 16:39:28 -0300

    feat: maquetado del navbar (con elementos "placeholder")

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
+import { NavLink } from "react-router"
+
 function Header() {
+    const navClass = "p-1 hover:font-bold active:font-bold"
     return (
         <header>
-            <h1>Header</h1>
+            <div className="flex justify-between p-4 bg-gray-950 text-amber-100">
+                <NavLink to='/'>Logo</NavLink>
+                <div>barra de busqueda</div>
+                <NavLink to='/carrito'>carrito</NavLink>
+            </div>
+            <div className="flex justify-start p-1 bg-gray-500 text-amber-100">
+                <NavLink to='/' className={navClass}>Inicio</NavLink>
+                <NavLink to='/catalogo' className={navClass}>Catálogo</NavLink>
+                <NavLink to='/catalogo/novedades' className={navClass}>Novedades</NavLink>
+                <NavLink to='/catalogo/ofertas' className={navClass}>Ofertas</NavLink>
+            </div>
         </header>
     )
 }
