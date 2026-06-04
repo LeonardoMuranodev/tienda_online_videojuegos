cf84807286e93a3dcbef26f48db2ac73fb583eb8
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-04 19:17:07 -0300

    feat: selección de destacados aleatoria y limitada a 5

--- src/components/ContainerProductos.jsx
+++ src/components/ContainerProductos.jsx
@@ -2,16 +2,19 @@ import CardProducto from "./Card";
 import productos from "../data/productos.json";
 
 export default function ContainerProductos() {
-  //const productosInicio = productos.slice(0, 6);
+
+  var sel = Math.floor(Math.random() * (productos.length - 10))
+
+  const productosDestacados = productos.slice(sel, sel + 5)
 
   return (
-    <section className="bg-zinc-950 py-14 px-6">
+    <section className="bg-gray-800 dark:bg-zinc-950 py-14 px-6">
       <h2 className="text-4xl font-bold text-white text-center mb-12">
         Juegos Destacados
       </h2>
 
       <div className="flex flex-wrap justify-center gap-8">
-        {productos.map((producto) => (
+        {productosDestacados.map((producto) => (
           <CardProducto key={producto.id} producto={producto} />
         ))}
       </div>
