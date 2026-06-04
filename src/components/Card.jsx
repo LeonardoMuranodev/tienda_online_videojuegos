f058e07cd68304c3380005eff9705a24c7f2bff0
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-04 19:16:44 -0300

    feat: implementación temas claro y oscuro, tamaño responsivo, y bugfix de gradient de fondo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,21 @@ import { Link } from "react-router";
 export default function CardProducto({ producto }) {
   return (
     <Card
-      className="w-48 bg-zinc-900 border border-zinc-700 rounded-xl overflow-hidden shadow-md hover:scale-105 transition-all duration-300"
+      className="sm:w-48 w-40 bg-blue-400 dark:bg-zinc-900 border border-zinc-700 rounded-xl overflow-hidden shadow-md hover:scale-105 transition-all duration-300"
       imgSrc={producto.imagenes.portada}
     >
-      <h5 className="text-sm font-bold text-white text-center leading-4 min-h-[40px]">
+      <h5 className="text-sm font-bold dark:text-gray-50 text-center leading-4 min-h-10">
         {producto.titulo}
       </h5>
 
       <div className="flex justify-center">
-        <span className="text-lg font-bold text-green-400">
+        <span className="text-lg font-bold text-green-600 dark:text-green-400">
           ${producto.precio}
         </span>
       </div>
 
       <Link to={`/producto/${producto.id}`}>
-        <Button size="xs" gradientDuoTone="purpleToBlue" className="w-full">
+        <Button size="xs" gradientDuoTone="purpleToBlue" className="w-full bg-linear-to-br from-purple-700 to-blue-700">
           Comprar
         </Button>
       </Link>
