97765e550d9fc979c9f63cc4bcd02cc07de78bed
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-05 17:12:24 -0300

    bugfix: /catalogo ahora muestra todo el catálogo en vez de solo los juegos destacados

--- src/components/ContainerProductos.jsx
+++ src/components/ContainerProductos.jsx
@@ -1,20 +1,26 @@
+import { useLocation } from "react-router";
+
 import CardProducto from "./Card";
 import productos from "../data/productos.json";
 
 export default function ContainerProductos() {
+  const location = useLocation();
 
   var sel = Math.floor(Math.random() * (productos.length - 10))
 
   const productosDestacados = productos.slice(sel, sel + 5)
 
+  const show = (location.pathname == '/catalogo') ? productos : productosDestacados;
+
+  console
   return (
     <section className="py-14 px-6">
       <h2 className="text-4xl font-bold text-white text-center mb-12">
-        Juegos Destacados
+        {(location.pathname == '/catalogo')? "Catálogo" : "Juegos destacados"}
       </h2>
 
       <div className="flex flex-wrap justify-center gap-8">
-        {productosDestacados.map((producto) => (
+        {show.map((producto) => (
           <CardProducto key={producto.id} producto={producto} />
         ))}
       </div>
