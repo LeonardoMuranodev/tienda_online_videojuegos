03e493d40bfd4228971864183e40aadeb494d71c
Matias-Ezeq <matiasezequiel.delarosa@estudiantes.unahur.edu.ar>
2026-06-05 15:53:33 -0300

    feat maquetado footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 function Footer() {
+    const fechaActual = new Date()
     return(
-        <footer className='absolute bottom-0'>
-            <p>ToDo: Footer</p>
+        <footer className='mt-auto bg-blue-900 dark:bg-gray-900 text-cyan-100 h-16'>
+            <div className="flex h-full items-center justify-between p-4">
+                <img src='src/data/logo-footer.svg' className="invert-100 h-8 fill-current dark:invert-0"/>
+                <p> Copyright {fechaActual.getFullYear()} - GameGarage</p>
+                <div>
+                    logos redes sociales
+                </div>
+            </div>
         </footer>
     )
 }
